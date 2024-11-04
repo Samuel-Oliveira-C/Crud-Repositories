18cd79823ca1d176f12e9f8af112208c21f4682f
Samuel Oliveira <samuel.oleiv@gmail.com>
2024-11-04 11:33:34 -0300

    Criado a estrutura do projeto

---
+++ crud_repositories/crud_repositories/src/main/java/com/crud/crud_repositories/CrudRepositoriesApplication.java
@@ -0,0 +1,13 @@
+package com.crud.crud_repositories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRepositoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRepositoriesApplication.class, args);
+	}
+
+}
---
+++ crud_repositories/crud_repositories/src/test/java/com/crud/crud_repositories/CrudRepositoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.crud_repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRepositoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
